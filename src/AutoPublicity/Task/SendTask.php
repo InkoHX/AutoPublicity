381284d8766e0b0ccb1025cd771a72cf5e969368
InkoHX <inkohx@gmail.com>
2018-09-26 15:44:52 +0900

    送信先変更

--- src/AutoPublicity/Task/SendTask.php
+++ src/AutoPublicity/Task/SendTask.php
@@ -23,7 +23,7 @@ class SendTask extends Task
     public function onRun(int $currentTick)
     {
         Main::getInstance()->getLogger()->alert("宣伝文の送信を開始");
-        $this->lobiapi->MakeThread("c832ba0bfb0771777e1b489bb1cf84d6d77419fd", Config::getSendMessage());
+        $this->lobiapi->MakeThread("514881fbce1d1866f468837df6d808e6fa8aa454", Config::getSendMessage());
         Main::getInstance()->getLogger()->info("送信が完了しました！");
         Main::getInstance()->getServer()->broadcastMessage(TextFormat::GREEN."[謝罪] 宣伝グループへ宣伝文を送信しました。ゲームのプレイに影響を与えてしまい申し訳ありません 開発者: InkoHX");
     }
